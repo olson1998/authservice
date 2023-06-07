f4b284795bca6fbcae8b4b4515028f0abd4e3a67
MSI <aleksander.inglot98@gmail.com>
2023-06-07 21:33:48 +0200

    feature/user controller

---
+++ src/main/java/com/olson1998/authdata/adapter/inbound/UserRequestController.java
@@ -0,0 +1,32 @@
+package com.olson1998.authdata.adapter.inbound;
+
+import com.olson1998.authdata.application.requesting.model.UserSavingAdapterRequest;
+import com.olson1998.authdata.domain.port.processing.pipeline.UserDatabaseOperationsPipeline;
+import com.olson1998.authdata.domain.port.processing.report.stereotype.UserDeletingReport;
+import com.olson1998.authdata.domain.port.processing.report.stereotype.UserSavingReport;
+import com.olson1998.authdata.domain.port.processing.request.stereotype.UserDeletingRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
+
+@RequiredArgsConstructor
+
+@Async
+@RestController
+@RequestMapping(value = "/user/data")
+public class UserRequestController {
+
+    private final UserDatabaseOperationsPipeline userDatabaseOperationsPipeline;
+
+    @PostMapping(path = "/post")
+    public CompletableFuture<UserSavingReport> interceptUserSavingRequest(@RequestBody UserSavingAdapterRequest userSavingAdapterRequest){
+        return userDatabaseOperationsPipeline.runSaveUserPipeline(userSavingAdapterRequest);
+    }
+
+    @DeleteMapping(path = "/del")
+    public CompletableFuture<UserDeletingReport> interceptUserDeletingRequest(@RequestBody UserDeletingRequest userDeletingRequest){
+        return userDatabaseOperationsPipeline.runUserDeletePipeline(userDeletingRequest);
+    }
+}
