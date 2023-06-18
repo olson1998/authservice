7b7ed3cf065623c951d843e7b3a9465afcad94b6
MSI <aleksander.inglot98@gmail.com>
2023-06-18 13:29:15 +0200

    bugfix/fixed not passing tests

--- src/test/java/com/olson1998/authdata/domain/service/processing/RoleRequestProcessingServiceTest.java
+++ src/test/java/com/olson1998/authdata/domain/service/processing/RoleRequestProcessingServiceTest.java
@@ -1,10 +1,179 @@
 package com.olson1998.authdata.domain.service.processing;
 
+import com.olson1998.authdata.domain.model.processing.request.LinkedAuthoritySavingRequest;
+import com.olson1998.authdata.domain.port.data.repository.AuthorityDataSourceRepository;
+import com.olson1998.authdata.domain.port.data.repository.RoleBindingDataSourceRepository;
+import com.olson1998.authdata.domain.port.data.repository.RoleDataSourceRepository;
+import com.olson1998.authdata.domain.port.data.stereotype.Role;
+import com.olson1998.authdata.domain.port.data.stereotype.RoleBinding;
+import com.olson1998.authdata.domain.port.processing.report.stereotype.AuthoritySavingReport;
+import com.olson1998.authdata.domain.port.processing.request.repository.AuthorityRequestProcessor;
+import com.olson1998.authdata.domain.port.processing.request.stereotype.AuthoritySavingRequest;
+import com.olson1998.authdata.domain.port.processing.request.stereotype.payload.AuthorityDetails;
+import com.olson1998.authdata.domain.port.processing.request.stereotype.payload.RoleBindingClaim;
+import com.olson1998.authdata.domain.port.processing.request.stereotype.payload.RoleDetails;
+import com.olson1998.authdata.domain.service.processing.request.RoleRequestProcessingService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.olson1998.authdata.application.datasource.entity.AuthorityTestDataSet.*;
+import static com.olson1998.authdata.application.datasource.entity.RoleBindingTestDataSet.*;
+import static com.olson1998.authdata.application.datasource.entity.RoleTestDataSet.*;
+import static com.olson1998.authdata.application.requesting.model.RoleRequestDataSet.*;
+import static com.olson1998.authdata.application.requesting.model.payload.AuthorityDetailsTestDataSet.*;
+import static java.util.Map.entry;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 @ExtendWith(MockitoExtension.class)
 class RoleRequestProcessingServiceTest {
 
+    private static final Set<RoleDetails> TEST_ROLE_DETAILS =
+            TEST_ROLE_SAVING_REQUEST.getDetails();
+
+    private static final Set<RoleBindingClaim> TEST_ROLE_BINDING_CLAIMS =
+            TEST_ROLE_BINDING_REQUEST.getRolesBindingsClaims();
+
+    private static final Set<AuthorityDetails> EXPECTED_LINKED_AUTHORITIES_TO_SAVE =
+            new LinkedAuthoritySavingRequest(TEST_ROLE_BINDING_REQUEST_WITH_SAVE_CLAIM)
+            .getAuthoritiesDetails();
+
+    private static final List<Role> MOCKED_PERSISTED_ROLES = List.of(
+            TEST_ROLE_DATA_1,
+            TEST_ROLE_DATA_2,
+            TEST_ROLE_DATA_3,
+            TEST_ROLE_DATA_4,
+            TEST_ROLE_DATA_5
+    );
+
+    private static final List<RoleBinding> MOCKED_PERSISTED_ROLE_BINDINGS = List.of(
+            TEST_ROLE_BINDING_1,
+            TEST_ROLE_BINDING_2,
+            TEST_ROLE_BINDING_3,
+            TEST_ROLE_BINDING_4,
+            TEST_ROLE_BINDING_5,
+            TEST_ROLE_BINDING_6
+    );
+
+    private static final Map<String, AuthorityDetails> MOCKED_SAVED_AUTHORITIES_MAP = Map.ofEntries(
+            entry(TEST_AUTHORITY_1_ID, TEST_AUTHORITY_DETAILS_FORM_1),
+            entry(TEST_AUTHORITY_2_ID, TEST_AUTHORITY_DETAILS_FORM_2),
+            entry(TEST_AUTHORITY_3_ID, TEST_AUTHORITY_DETAILS_FORM_3)
+    );
+
+    @Mock
+    private AuthorityRequestProcessor authorityRequestProcessor;
+
+    @Mock
+    private RoleDataSourceRepository roleDataSourceRepository;
+
+    @Mock
+    private RoleBindingDataSourceRepository roleBindingDataSourceRepository;
+
+    @Mock
+    private AuthorityDataSourceRepository authorityDataSourceRepository;
+
+    @Mock
+    private AuthoritySavingReport authoritySavingReport;
+
+    @Captor
+    private ArgumentCaptor<AuthoritySavingRequest> authoritySavingRequestCaptor;
+
+    @Test
+    void shouldSaveGivenRoleDetails(){
+        roleRequestProcessingService().saveNewRoles(TEST_ROLE_SAVING_REQUEST);
+
+        then(roleDataSourceRepository).should().saveRoles(TEST_ROLE_DETAILS);
+    }
+
+    @Test
+    void shouldReturnReportBasedOnRequestDetails(){
+        given(roleDataSourceRepository.saveRoles(anySet()))
+                .willReturn(MOCKED_PERSISTED_ROLES);
+
+        var report =
+                roleRequestProcessingService().saveNewRoles(TEST_ROLE_SAVING_REQUEST);
+
+        assertThat(report.getRequestId()).isEqualTo(TEST_ROLE_SAVING_REQUEST_ID);
+        assertThat(report.getPersistedRolesDetailsMap()).containsOnlyKeys(
+                List.of(TEST_ROLE_1_ID, TEST_ROLE_2_ID, TEST_ROLE_3_ID ,TEST_ROLE_4_ID, TEST_ROLE_5_ID)
+        );
+    }
+
+    @Test
+    void shouldSaveGivenRoleBindings(){
+        roleRequestProcessingService().saveNewRoleBounds(TEST_ROLE_BINDING_REQUEST);
+
+        then(roleBindingDataSourceRepository).should().saveRoleBindings(TEST_ROLE_BINDING_CLAIMS);
+    }
+
+    @Test
+    void shouldNotSaveAnyAuthoritiesIfRequestAuthoritySaveRequestClaimIsEmpty(){
+        assertThat(TEST_ROLE_BINDING_REQUEST.getRoleIdAuthoritySavingRequestMap()).isEmpty();
+
+        roleRequestProcessingService().saveNewRoleBounds(TEST_ROLE_BINDING_REQUEST);
+
+        then(authorityDataSourceRepository).shouldHaveNoInteractions();
+    }
+
+    @Test
+    void shouldReturnSetOfBoundedRolesIdsAndAuthoritiesId(){
+        given(roleBindingDataSourceRepository.saveRoleBindings(anySet()))
+                .willReturn(MOCKED_PERSISTED_ROLE_BINDINGS);
+        given(roleDataSourceRepository.updateRoleTimestamp(anySet(), anyLong()))
+                .willReturn(getMockedPersistedRoleBindingsRolesQty());
+
+        var report =
+                roleRequestProcessingService().saveNewRoleBounds(TEST_ROLE_BINDING_REQUEST);
+
+        var savedRoleBindings = report.getSavedRoleBindings();
+        assertThat(savedRoleBindings).containsOnlyKeys(List.of(TEST_ROLE_1_ID, TEST_ROLE_2_ID, TEST_ROLE_3_ID));
+        assertThat(savedRoleBindings.get(TEST_ROLE_1_ID)).containsExactlyInAnyOrder(TEST_AUTHORITY_1_ID, TEST_AUTHORITY_2_ID);
+        assertThat(savedRoleBindings.get(TEST_ROLE_2_ID)).containsExactlyInAnyOrder(TEST_AUTHORITY_1_ID, TEST_AUTHORITY_2_ID);
+        assertThat(savedRoleBindings.get(TEST_ROLE_3_ID)).containsExactlyInAnyOrder(TEST_AUTHORITY_1_ID, TEST_AUTHORITY_2_ID);
+    }
+
+    @Test
+    void shouldSaveLinkedAuthoritiesIfAnyPresent(){
+        given(authorityRequestProcessor.saveAuthorities(any()))
+                .willReturn(authoritySavingReport);
+        given(authoritySavingReport.getPersistedAuthoritiesDetailsMap())
+                .willReturn(MOCKED_SAVED_AUTHORITIES_MAP);
+
+        roleRequestProcessingService().saveNewRoleBounds(TEST_ROLE_BINDING_REQUEST_WITH_SAVE_CLAIM);
+
+        then(authorityRequestProcessor).should().saveAuthorities(authoritySavingRequestCaptor.capture());
+
+        var authoritySavingReq = authoritySavingRequestCaptor.getValue();
+
+        assertThat(authoritySavingReq.getAuthoritiesDetails()).containsAll(EXPECTED_LINKED_AUTHORITIES_TO_SAVE);
+    }
+
+    private RoleRequestProcessingService roleRequestProcessingService(){
+        return new RoleRequestProcessingService(
+                authorityRequestProcessor,
+                roleDataSourceRepository,
+                roleBindingDataSourceRepository
+        );
+    }
+
+
+    private int getMockedPersistedRoleBindingsRolesQty(){
+        return MOCKED_PERSISTED_ROLE_BINDINGS.stream()
+                .map(RoleBinding::getRoleId)
+                .collect(Collectors.toSet())
+                .size();
+    }
 
 }
--- src/test/java/com/olson1998/authdata/domain/service/processing/UserRequestProcessingServiceTest.java
+++ src/test/java/com/olson1998/authdata/domain/service/processing/UserRequestProcessingServiceTest.java
@@ -1,9 +1,114 @@
 package com.olson1998.authdata.domain.service.processing;
 
+import com.olson1998.authdata.domain.port.data.exception.RollbackRequiredException;
+import com.olson1998.authdata.domain.port.data.repository.UserDataSourceRepository;
+import com.olson1998.authdata.domain.port.data.repository.UserMembershipDataSourceRepository;
+import com.olson1998.authdata.domain.port.processing.request.repository.RoleRequestProcessor;
+import com.olson1998.authdata.domain.port.processing.request.stereotype.payload.UserDetails;
+import com.olson1998.authdata.domain.port.processing.request.stereotype.payload.UserMembershipClaim;
+import com.olson1998.authdata.domain.service.processing.request.UserRequestProcessingService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Set;
+
+import static com.olson1998.authdata.application.datasource.entity.UserTestDataSet.*;
+import static com.olson1998.authdata.application.requesting.model.UserRequestDataSet.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 @ExtendWith(MockitoExtension.class)
 class UserRequestProcessingServiceTest {
 
+    private static final UserDetails TEST_USER_SAVE_REQUEST_DETAILS =
+            TEST_USER_SAVING_REQUEST.getUserDetails();
+
+    private static final Set<UserMembershipClaim> TEST_USER_MEMBERSHIP_CLAIMS =
+            TEST_USER_SAVING_REQUEST_WITH_MEMBERSHIP_CLAIM.getMembershipClaims();
+
+    @Mock
+    private RoleRequestProcessor roleRequestProcessor;
+
+    @Mock
+    private UserDataSourceRepository userDataSourceRepository;
+
+    @Mock
+    private UserMembershipDataSourceRepository userMembershipDataSourceRepository;
+
+    @Test
+    void shouldSaveUserUsingGivenUserDetails() throws RollbackRequiredException {
+        given(userDataSourceRepository.saveUser(TEST_USER_SAVE_REQUEST_DETAILS))
+                .willReturn(TEST_USER_DATA);
+
+        userRequestProcessingService().saveUser(TEST_USER_SAVING_REQUEST);
+
+        then(userDataSourceRepository).should().saveUser(TEST_USER_SAVE_REQUEST_DETAILS);
+    }
+
+    @Test
+    void shouldSaveUserMembershipClaims(){
+        given(userDataSourceRepository.saveUser(any()))
+                .willReturn(TEST_USER_DATA);
+
+        userRequestProcessingService().saveUser(TEST_USER_SAVING_REQUEST_WITH_MEMBERSHIP_CLAIM);
+
+        then(userMembershipDataSourceRepository).should().saveUserMemberships(TEST_USER_DATA.getId(), TEST_USER_MEMBERSHIP_CLAIMS);
+    }
+
+    @Test
+    void shouldIncludePersistedDataIntoReport(){
+        given(userDataSourceRepository.saveUser(any()))
+                .willReturn(TEST_USER_DATA);
+
+        var report =
+                userRequestProcessingService().saveUser(TEST_USER_SAVING_REQUEST_WITH_MEMBERSHIP_CLAIM);
+
+        assertThat(report.getUserId()).isEqualTo(TEST_USER_ID);
+        assertThat(report.getUsername()).isEqualTo(TEST_USER_USERNAME);
+        assertThat(report.getRequestId()).isEqualTo(TEST_USER_SAVING_REQUEST_ID );
+    }
+
+    @Test
+    void shouldDeleteUserMembershipsAllPrivateRolesForAndUserDetailsGivenUserId(){
+        given(userDataSourceRepository.deleteUser(TEST_USER_ID))
+                .willReturn(1);
+
+        userRequestProcessingService().deleteUser(TEST_USER_DELETING_REQUEST);
+
+        then(userMembershipDataSourceRepository).should().deleteAllUserMemberships(TEST_USER_ID);
+        then(roleRequestProcessor).should().deleteUserRoles(TEST_USER_DELETING_REQUEST);
+        then(userDataSourceRepository).should().deleteUser(TEST_USER_ID);
+    }
+
+    @Test
+    void shouldThrowRollbackRequiredExceptionIfRepositoryReturnedZeroDeletedUsers(){
+        given(userDataSourceRepository.deleteUser(TEST_USER_ID))
+                .willReturn(0);
+
+        assertThatExceptionOfType(RollbackRequiredException.class).isThrownBy(()->{
+            userRequestProcessingService().deleteUser(TEST_USER_DELETING_REQUEST);
+        });
+    }
+
+    @Test
+    void shouldSaveUserMemberships(){
+        userRequestProcessingService().bindMemberships(TEST_USER_MEMBERSHIP_SAVING_REQUEST);
+
+        then(userMembershipDataSourceRepository).should().saveUserMemberships(
+                TEST_USER_ID,
+                TEST_USER_MEMBERSHIP_SAVING_REQUEST.getUserMembershipClaims()
+        );
+    }
+    private UserRequestProcessingService userRequestProcessingService(){
+        return new UserRequestProcessingService(
+                roleRequestProcessor,
+                userDataSourceRepository,
+                userMembershipDataSourceRepository
+        );
+    }
 }
