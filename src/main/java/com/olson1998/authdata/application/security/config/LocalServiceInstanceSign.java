369b258a245059adb40915d3b6c24a5a26e9f2ef
MSI <aleksander.inglot98@gmail.com>
2023-06-24 07:50:53 +0200

    feature/local service sign

---
+++ src/main/java/com/olson1998/authdata/application/security/config/LocalServiceInstanceSign.java
@@ -0,0 +1,29 @@
+package com.olson1998.authdata.application.security.config;
+
+import com.olson1998.authdata.domain.port.security.stereotype.ServiceInstanceSign;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+
+@Slf4j
+
+@Getter
+@Component
+public class LocalServiceInstanceSign implements ServiceInstanceSign {
+
+    private final String value;
+
+    public LocalServiceInstanceSign(Environment env) {
+        var inetAddress = InetAddress.getLoopbackAddress().getHostAddress();
+        var port = env.getProperty("server.port", Integer.TYPE);
+        var signBuild = new StringBuilder(inetAddress);
+        if(port != null){
+            signBuild.append(':').append(port);
+        }
+        this.value = signBuild.toString();
+        log.info("Microservice will sig as '{}'", this.value);
+    }
+}
---
+++ src/main/java/com/olson1998/authdata/domain/port/security/stereotype/ServiceInstanceSign.java
@@ -0,0 +1,6 @@
+package com.olson1998.authdata.domain.port.security.stereotype;
+
+public interface ServiceInstanceSign {
+
+    String getValue();
+}
